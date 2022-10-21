1cc2080c640c95c3a289b5bdc45dd25e4e147cdb
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-10-20 18:12:03 -0700

    test(packages/remark-tight-comments): ensure unit tests are passing

--- packages/remark-tight-comments/test/unit-index.test.ts
+++ packages/remark-tight-comments/test/unit-index.test.ts
@@ -1,99 +1,19 @@
 import { remark } from 'remark';
-import remarkReferenceLinks from 'remark-reference-links';
-import remarkRemoveComments from 'remark-remove-comments';
+import remarkTightComments from 'pkgverse/remark-tight-comments/src/index';
 
-import remarkIgnore, {
-  ignoreStart,
-  ignoreEnd
-} from 'pkgverse/remark-tight-comments/src/index';
-import defaultIgnoreStart from 'pkgverse/remark-tight-comments/src/start';
-import defaultIgnoreEnd from 'pkgverse/remark-tight-comments/src/end';
 import {
   getFixtureString,
   getFixtureVFile
 } from 'pkgverse/remark-tight-comments/test/helpers';
 
 describe('::default', () => {
-  it('is no-op without ignore commands', async () => {
+  it('most comments have newlines removed with respect to pipe syntax', async () => {
     expect.hasAssertions();
 
     const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('clean'));
+      .use(remarkTightComments)
+      .process(await getFixtureVFile('spaced'));
 
-    expect(result.toString()).toStrictEqual(getFixtureString('clean-transformed'));
-  });
-
-  it('ignores the next node via remark-tight-comments', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('ignore-next'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('ignore-next-transformed'));
-  });
-
-  it('ignores a range of nodes via remark-tight-comments-start/end', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('ignore-range'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('ignore-range-transformed'));
-  });
-
-  it('does not malfunction on orphan remark-tight-comments-end', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('orphan'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('orphan-transformed'));
-  });
-
-  it('does not malfunction on redundant remark-tight-comments', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('redundant'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('redundant-transformed'));
-  });
-});
-
-describe('::ignoreStart & ::ignoreEnd', () => {
-  it('only disables transforms that occur between ignoreStart and ignoreEnd', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(ignoreStart)
-      .use(remarkReferenceLinks)
-      .use(ignoreEnd)
-      .use(remarkRemoveComments)
-      .process(await getFixtureVFile('ignore-range-2'));
-
-    expect(result.toString()).toStrictEqual(
-      getFixtureString('ignore-range-2-transformed')
-    );
-
-    const result2 = await remark()
-      .use(defaultIgnoreStart)
-      .use(remarkReferenceLinks)
-      .use(defaultIgnoreEnd)
-      .use(remarkRemoveComments)
-      .process(await getFixtureVFile('ignore-range-2'));
-
-    expect(result2.toString()).toStrictEqual(
-      getFixtureString('ignore-range-2-transformed')
-    );
+    expect(result.toString()).toStrictEqual(getFixtureString('spaced-transformed'));
   });
 });
