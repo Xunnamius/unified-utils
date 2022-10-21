9ea3a4615b751d0bb9f9292fae22e48cfac42fcb
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-10-20 18:11:01 -0700

    test(packages/mdast-util-tight-comments): ensure all tests are passing

--- packages/mdast-util-tight-comments/test/helpers.ts
+++ packages/mdast-util-tight-comments/test/helpers.ts
@@ -1,12 +1,17 @@
 import { readFileSync as readToString } from 'fs';
-import { read as readToVFile } from 'to-vfile';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import { toMarkdown, type Options } from 'mdast-util-to-markdown';
 
-export function getFixtureVFile(fixture: string) {
-  return readToVFile(`${__dirname}/fixtures/${fixture}.md`);
-}
+import type { Root } from 'mdast';
 
-export function getFixtureString(fixture: string, { trim = true } = {}) {
+export function getFixtureString(fixture: string, { trim = false } = {}) {
   return readToString(`${__dirname}/fixtures/${fixture}.md`, 'utf8')[
     trim ? 'trim' : 'toString'
   ]();
+}
+
+export function transformString(str: string, { toMarkdownOptions = {} as Options } = {}) {
+  const tree = unified().use(remarkParse).parse(str);
+  return toMarkdown(tree as Root, toMarkdownOptions);
 }
--- packages/mdast-util-tight-comments/test/unit-index.test.ts
+++ packages/mdast-util-tight-comments/test/unit-index.test.ts
@@ -1,99 +1,36 @@
-import { remark } from 'remark';
-import remarkReferenceLinks from 'remark-reference-links';
-import remarkRemoveComments from 'remark-remove-comments';
-
-import remarkIgnore, {
-  ignoreStart,
-  ignoreEnd
-} from 'pkgverse/mdast-util-tight-comments/src/index';
-import defaultIgnoreStart from 'pkgverse/mdast-util-tight-comments/src/start';
-import defaultIgnoreEnd from 'pkgverse/mdast-util-tight-comments/src/end';
 import {
   getFixtureString,
-  getFixtureVFile
+  transformString
 } from 'pkgverse/mdast-util-tight-comments/test/helpers';
 
-describe('::default', () => {
-  it('is no-op without ignore commands', async () => {
+import { joinTightComments } from 'pkgverse/mdast-util-tight-comments/src/index';
+
+describe('::joinTightComments', () => {
+  it('comments have newlines inserted before and after without joiner', async () => {
     expect.hasAssertions();
 
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('clean'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('clean-transformed'));
-  });
-
-  it('ignores the next node via mdast-util-tight-comments', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('ignore-next'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('ignore-next-transformed'));
-  });
-
-  it('ignores a range of nodes via mdast-util-tight-comments-start/end', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('ignore-range'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('ignore-range-transformed'));
-  });
-
-  it('does not malfunction on orphan mdast-util-tight-comments-end', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('orphan'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('orphan-transformed'));
-  });
-
-  it('does not malfunction on redundant mdast-util-tight-comments', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(remarkIgnore)
-      .use(remarkReferenceLinks)
-      .process(await getFixtureVFile('redundant'));
-
-    expect(result.toString()).toStrictEqual(getFixtureString('redundant-transformed'));
-  });
-});
-
-describe('::ignoreStart & ::ignoreEnd', () => {
-  it('only disables transforms that occur between ignoreStart and ignoreEnd', async () => {
-    expect.hasAssertions();
-
-    const result = await remark()
-      .use(ignoreStart)
-      .use(remarkReferenceLinks)
-      .use(ignoreEnd)
-      .use(remarkRemoveComments)
-      .process(await getFixtureVFile('ignore-range-2'));
-
-    expect(result.toString()).toStrictEqual(
-      getFixtureString('ignore-range-2-transformed')
+    expect(transformString(getFixtureString('clean'))).toStrictEqual(
+      getFixtureString('clean-transformed')
     );
+  });
 
-    const result2 = await remark()
-      .use(defaultIgnoreStart)
-      .use(remarkReferenceLinks)
-      .use(defaultIgnoreEnd)
-      .use(remarkRemoveComments)
-      .process(await getFixtureVFile('ignore-range-2'));
+  it('most comments have newlines removed with joiner', async () => {
+    expect.hasAssertions();
 
-    expect(result2.toString()).toStrictEqual(
-      getFixtureString('ignore-range-2-transformed')
-    );
+    expect(
+      transformString(getFixtureString('tightened'), {
+        toMarkdownOptions: joinTightComments()
+      })
+    ).toStrictEqual(getFixtureString('tightened-transformed'));
+  });
+
+  it('joiner respects pipe syntax', async () => {
+    expect.hasAssertions();
+
+    expect(
+      transformString(getFixtureString('spaced'), {
+        toMarkdownOptions: joinTightComments()
+      })
+    ).toStrictEqual(getFixtureString('spaced-transformed'));
   });
 });
